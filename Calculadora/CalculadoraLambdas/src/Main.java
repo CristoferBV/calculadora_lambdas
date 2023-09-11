6dc8c7a6e2942269ae03369a88f74563b3dee23c
CristoferBV <70454042+CristoferBV@users.noreply.github.com>
2023-09-11 10:37:28 -0600

    Update main

--- Calculadora/CalculadoraLambdas/src/Main.java
+++ Calculadora/CalculadoraLambdas/src/Main.java
@@ -1,16 +1,68 @@
 
-
 /**
  *
  * @author Cristofer
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int opc = 0;
+        
+        //Menu de la calculadora
+        switch (opc) {
+            case 1:
+                sumar();
+                break;
+            case 2:
+                restar();
+                break;
+            case 3:
+                multiplicar();
+                break;
+            case 4:
+                dividir();
+                break;
+
+            case 5:
+                potencia();
+                break;
+            case 6:
+                raiz();
+                break;
+            case 7:
+                factorial();
+                break;
+            default:
+                break;
+        }
     }
-    
+
+    private static void sumar() {
+        
+    }
+
+    private static void multiplicar() {
+        
+    }
+
+    private static void restar() {
+      
+    }
+
+    private static void dividir() {
+       
+    }
+
+    private static void potencia() {
+       
+    }
+
+    private static void raiz() {
+        
+    }
+
+    private static void factorial() {
+      
+    }
+
 }
